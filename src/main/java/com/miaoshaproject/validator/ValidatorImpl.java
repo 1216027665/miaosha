a703c3855d90a60a0c5df3d28b3febc7bda94697
wangyouliang <1216027665@qq.com>
2020-03-12 14:59:06 +0800

    提交项目src源代码

---
+++ src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
@@ -0,0 +1,40 @@
+package com.miaoshaproject.validator;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Component
+public class ValidatorImpl implements InitializingBean{
+
+    private Validator validator;
+
+    //实现校验方法并返回校验结果
+    public ValidationResult validate(Object bean){
+        final ValidationResult result = new ValidationResult();
+
+        //如果对应的bean里边的一些参数规则，有违背了对应Validation定义的annotation的话，这个set里边就会有这个值。
+        Set<ConstraintViolation<Object>> constraintViolationSet = validator.validate(bean);
+        if(constraintViolationSet.size() > 0){
+            //有错误
+            result.setHasErrors(true);
+            constraintViolationSet.forEach(constraintViolation->{
+                String errMsg = constraintViolation.getMessage();
+                String propertyName = constraintViolation.getPropertyPath().toString();
+                result.getErrorMsgMap().put(propertyName, errMsg);
+            });
+        }
+        return result;
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //将hibernate validator通过工厂的初始化方式使其实例化
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+}
